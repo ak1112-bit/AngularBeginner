3f1349eacdb42874dbd84c3603b4e0ee92861c77
ak1112-bit <aknttk669@gmail.com>
2020-04-10 14:27:22 +0900

    inputバリデーション　※赤くなるだけ

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormsModule, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormsModule, FormControl, Validators, FormGroup, AbstractControl } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -9,35 +9,46 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items;
-  checkoutForm;
-
+  // checkoutForm;
+  editForm: FormGroup;
 
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder,
     private formModule: FormsModule,
+    // private formGroup: FormGroup,
   ) {
-    this.checkoutForm = this.formBuilder.group({
-      name: '',
-      address: '',
-    });
+    // this.checkoutForm = this.formBuilder.group({
+    //   name: '',
+    //   address: ['', [Validators.email]],
+    // });
    }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+    this.editForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      address: ['', [Validators.email]],
+    });
   }
 
   onSubmit(customerData) {
     this.items = this.cartService.clearCart();
-    this.checkoutForm.reset();
+    this.editForm.reset();
 
-    console.warn('Your order has been submitted', customerData);
+    console.warn('注文が完了しました', customerData);
   }
   // getErrorMessage() {
-  //   if (this.email.hasError('required')) {
-  //     return 'You must enter a value';
+  //   if (this.checkoutForm.address.hasError('required')) {
+  //     return 'メールアドレスを入力してください';
   //   }
 
-  //   return this.email.hasError('email') ? 'Not a valid email' : '';
+  //   return this.checkoutForm.address.hasError('address') ? 'Not a valid email' : '';
   // }
+  get name(): AbstractControl {
+    return this.editForm.get('name');
+  }
+  get address(): AbstractControl {
+    return this.editForm.get('address');
+  }
 }
